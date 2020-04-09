ccb12fd46716da591f0b5407323fcefb903b8b53
Róbert Lexmann <rlexmann@gmail.com>
2020-04-09 10:28:18 +0200

    Create feed.py
    Does nothing, just exits if there is another running instance.

---
+++ feed.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import fcntl
+fh=0
+def run_once():
+    global fh
+    fh=open(os.path.realpath(__file__),'r')
+    try:
+        fcntl.flock(fh,fcntl.LOCK_EX|fcntl.LOCK_NB)
+    except:
+        os._exit(0)
+
+run_once()
